c3d2d0d8c43fe55972087b9cc4641b7996fed4ec
Kumar <guyfromchennai@gmail.com>
2023-12-12 11:13:14 +0530

    added and fixed methods

--- twosum.ts
+++ twosum.ts
@@ -1,17 +1,4 @@
 function twoSum(nums: number[], target: number): number[] {
-  // let result:number[] = [];
-  // for(let i=0; i< nums.length; i++){
-  //     const next = i+1;
-  //     console.log(i, next)
-  //     console.log(nums[i],nums[next])
-  //     if(target == nums[i]+nums[next]) {
-  //         result.push(i);
-  //         result.push(next);
-  //     }
-  // }
-
-  // return result;
-
   let result: number[] = [];
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
@@ -26,21 +13,26 @@ function twoSum(nums: number[], target: number): number[] {
   return result;
 }
 
+// console.log("twosum", tsum);
+// const tsum = twoSum1([3, 2, 3], 6); // [ 0, 2 ]
+const tsum = twoSum([2, 7, 11, 15], 9);
+console.log("tsum", tsum); // outputs [ 0, 1 ]
+
+/**
+ * This is working, but the testcases in Leetcode will not pass
+ * For coding interview this is ok
+ */
 function twoSum1(nums: number[], target: number): number[] {
-  const map = new Map();
-
-  for (let i = 0; i < nums.length; i += 1) {
-    const num = nums[i];
-    const diff = target - num;
-
-    if (map.has(diff)) {
-      return [map.get(diff), i];
+  let result: number[] = [];
+  for (let i = 0; i < nums.length; i++) {
+    const next = i + 1;
+    console.log(i, next);
+    console.log(nums[i], nums[next]);
+    if (target == nums[i] + nums[next]) {
+      result.push(i);
+      result.push(next);
     }
-
-    map.set(num, i);
   }
-}
 
-const tsum = twoSum1([2, 7, 11, 15], 9);
-// const tsum = twoSum([3, 2, 3], 6);
-console.log("twosum", tsum);
+  return result;
+}
