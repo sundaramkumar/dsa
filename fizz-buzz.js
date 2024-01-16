a77cbe0f6e8f36120dc3ff859ef4bdc96f85a27f
sundaramkumar <guyfromchennai@gmail.com>
2024-01-16 18:36:00 +0530

    FizzBuzz

---
+++ fizz-buzz.js
@@ -0,0 +1,47 @@
+/**
+ * FizzBuzz
+ *
+ * https://leetcode.com/problems/fizz-buzz/submissions/1147815005/
+ *
+ * Given an integer n, return a string array answer (1-indexed) where:
+ *
+ * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ * answer[i] == "Fizz" if i is divisible by 3.
+ * answer[i] == "Buzz" if i is divisible by 5.
+ * answer[i] == i (as a string) if none of the above conditions are true.
+ *
+ * Example 1:
+ *
+ * Input: n = 3
+ * Output: ["1","2","Fizz"]
+ * Example 2:
+ *
+ * Input: n = 5
+ * Output: ["1","2","Fizz","4","Buzz"]
+ * Example 3:
+ *
+ * Input: n = 15
+ * Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ *
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  result = [];
+  for (var i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push("" + i);
+    }
+  }
+  console.log(result);
+  return result;
+};
+fizzBuzz(3);
+fizzBuzz(5);
+fizzBuzz(15);
